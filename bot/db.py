19c40b0295be08f8b298b4400060f2c6aeaf5b6a
LevZed <lev27012000@gmail.com>
2022-07-29 20:05:42 +0500

    fix

--- bot/db.py
+++ bot/db.py
@@ -1,28 +1,12 @@
 import sqlite3
 import datetime
 import pytz
-import psycopg2
 import json
-
-# a = "postgres://yejddozsfnklsi:7441bc9fb0c8aca1d40b3a994d82075f2e3724330eb3ed65b4213d4b97686f93@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/df1tdsvbhu6ra8"
-# db_connection = psycopg2.connect(a, sslmode='require')
-# cur = db_connection.cursor()
 
 
 def get_now():
     return datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%d-%m-%Y')
 
-
-# def check_user(user_id, username, first_name):
-#     cur.execute(f"SELECT id FROM users WHERE id = {user_id}")
-#     result = cur.fetchone()
-
-#     if not result:
-#         cur.execute("INSERT INTO users (id, username, first_name) VALUES (%s, %s, %s)",
-#                     (user_id, username, first_name))
-#         db_connection.commit()
-#         return False
-#     return True
 
 locCon = sqlite3.connect('bot/local.db', check_same_thread=False)
 cur = locCon.cursor()
@@ -71,8 +55,6 @@ def check_transaction(hash):
     if result:
         return True
     return False
-
-# get list of transactions for wallet
 
 
 def get_user_payments(user_id):
